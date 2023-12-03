97298d9f0c4e922e5abc7c230df01fcb807cf3eb
Lakret <rakuretu@gmail.com>
2023-12-03 23:11:29 +0100

    day 3, part 2

--- d03.jl
+++ d03.jl
@@ -21,16 +21,21 @@ function add_digit!(s::State, digit::Char, pos::CartesianIndex{2})
   push!(s.positions, pos)
 end
 
-function push_if_part_number!(part_numbers::Vector{Int64}, state::State, symbol_positions::Set{CartesianIndex{2}})
+function push_if_part_number!(
+  part_numbers::Vector{Tuple{Int64,Set{CartesianIndex}}},
+  state::State,
+  symbol_positions::Set{CartesianIndex{2}}
+)
   candidate = @pipe String(state.digits) |> parse(Int64, _)
   all_neighbour_positions = map(pos -> [pos] .+ neighbour_deltas, state.positions) |> Iterators.flatten
-  if !(intersect(symbol_positions, all_neighbour_positions) |> isempty)
-    push!(part_numbers, candidate)
+  neighbouring_symbol_positions = intersect(symbol_positions, all_neighbour_positions)
+  if !isempty(neighbouring_symbol_positions)
+    push!(part_numbers, (candidate, neighbouring_symbol_positions))
   end
 end
 
 function part_numbers(inp)
-  part_numbers = Vector{Int64}()
+  part_numbers = Vector{Tuple{Int64,Set{CartesianIndex}}}()
   symbol_positions = findall(issymbol, inp) |> Set
   # sort them in a row-by-row order, so we can rely on digits of one number following each other
   digit_positions = @pipe findall(isdigit, inp) |> sort(_, by=x -> (x[1], x[2]))
@@ -53,7 +58,28 @@ function part_numbers(inp)
   part_numbers
 end
 
-p1(inp) = part_numbers(inp) |> sum
+p1(inp) = part_numbers(inp) .|> first |> sum
+
+function gear_numbers(inp)
+  gear_symbol_positions = findall(ch -> ch == '*', inp)
+
+  candidate_gears = Dict{CartesianIndex{2},Vector{Int64}}()
+  for (part_number, adjacent_symbol_positions) = part_numbers(inp)
+    adjacent_gear_symbol_positions = intersect(adjacent_symbol_positions, gear_symbol_positions)
+
+    for candidate_gear_position = adjacent_gear_symbol_positions
+      pns = get(candidate_gears, candidate_gear_position, [])
+      push!(pns, part_number)
+
+      candidate_gears[candidate_gear_position] = pns
+    end
+  end
+
+  candidate_gears = values(candidate_gears) |> collect
+  filter(numbers -> length(numbers) == 2, candidate_gears)
+end
+
+p2(inp) = gear_numbers(inp) .|> prod |> sum
 
 test_inp = """467..114..
   ...*......
@@ -71,3 +97,6 @@ input = readchomp("inputs/d03") |> parse_input
 
 @assert p1(test_inp) == 4361
 @assert @show p1(input) == 540212
+
+@assert p2(test_inp) == 467835
+@assert @show p2(input) == 87605697

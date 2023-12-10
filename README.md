31502bd58319f419f7b5d7ff2d60faa0c297db34
lakret <Rakuretu@gmail.com>
2023-12-10 18:23:22 +0100

    d09

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 Solutions in Elixir, Rust, Julia, and Python.
 
-We are not allowed to share the inputs, but the code in this repo assumes that they are placed in the `inputs` folder and named `d01`, `d02`, etc.
+We are not allowed to share the inputs, but the code in this repo assumes that they are placed in the `inputs` folder
+and named `d01`, `d02`, etc.
---
+++ d09.jl
@@ -0,0 +1,43 @@
+using Pipe
+
+parse_input(input) = @pipe strip(input) |> split(_, "\n") |> split.(_) |> map(x -> parse.(Int64, x), _)
+
+test_history = parse_input("""
+  0 3 6 9 12 15
+  1 3 6 10 15 21
+  10 13 16 21 30 45
+  """)
+history = readchomp("inputs/d09") |> parse_input
+
+function predict(value_history; forward=true)
+  sequence = deepcopy(value_history)
+  sequences = [sequence]
+
+  while !all(x -> x == 0, sequence)
+    sequence = @pipe [sequence[i+1] - sequence[i] for i in eachindex(sequence) if i < length(sequence)]
+    push!(sequences, sequence)
+  end
+
+  for i in (length(sequences)-1):-1:1
+    seq = sequences[i]
+
+    if forward
+      push!(seq, last(seq) + last(sequences[i+1]))
+    else
+      pushfirst!(seq, first(seq) - first(sequences[i+1]))
+    end
+  end
+
+  forward ? last(sequences[1]) : first(sequences[1])
+end
+
+p1(history) = predict.(history) |> sum
+p2(history) = predict.(history; forward=false) |> sum
+
+@assert predict.(test_history) == [18, 28, 68]
+@assert p1(test_history) == 114
+@time @assert @show p1(history) == 1930746032
+
+@assert predict.(test_history; forward=false) == [-3, 0, 5]
+@assert p2(test_history) == 2
+@time @assert @show p2(history) == 1154
